ec0459303db11804c392c13775fa0f0107508771
SampleXXX <70417664+SampleXXX@users.noreply.github.com>
2022-11-03 23:05:33 -0400

    Update controller.js

--- javascript/controller.js
+++ javascript/controller.js
@@ -8,34 +8,34 @@ let css = document.createElement('link');
 css.rel = "stylesheet";
 css.type ="text/css";
 css.id = cssID;
-css.href = "/css/home.css";
+css.href = "css/home.css";
 document.head.appendChild(css);
 
 let js = document.createElement('script');
-js.src = "/javascript/home.js";
+js.src = "javascript/home.js";
 document.body.appendChild(js);
 
 function home() {
     if (currentPage != 1)
-        document.getElementById(cssID).href = "/css/home.css";
+        document.getElementById(cssID).href = "css/home.css";
 
-        js.src = "/javascript/home.js";
+        js.src = "javascript/home.js";
         currentPage = 1;
     }
 
 
 function contact() {
         if (currentPage != 3){
-            document.getElementById(cssID).href = "/css/contact.css";
-            js.src = "/javascript/contact.js";
+            document.getElementById(cssID).href = "css/contact.css";
+            js.src = "javascript/contact.js";
         
             currentPage = 3;
         }
 }
 function about() {
     if (currentPage != 2){
-        document.getElementById(cssID).href = "/css/about.css";
-        js.src = '/javscript/about.js';
+        document.getElementById(cssID).href = "css/about.css";
+        js.src = 'javscript/about.js';
         currentPage = 2;
 
     }
